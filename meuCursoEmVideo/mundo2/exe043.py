2b6091bd1ec2e25fcf0466e2e59052bf3149e921
Felipe Lara <69820156+FelipeLaras@users.noreply.github.com>
2021-03-09 14:41:23 -0300

    Update exe043.py

--- meuCursoEmVideo/mundo2/exe043.py
+++ meuCursoEmVideo/mundo2/exe043.py
@@ -2,13 +2,13 @@ print('calculando o IMC')
 print('-' * 20)
 
 #dados sendo informado pelo usuário
-altura = float(input('Informe sua altura: '))
-peso = float(input('Informe seu peso'))
+
+peso = float(input('Informe seu peso(kg): '))
+altura = float(input('Informe sua altura(Metro): '))
 
 
 #realizando o calculo
-
-imc = (altura * altura) / peso
+imc = peso / (altura ** 2)# altura ao quadrado
 
 
 #Abaixo de 18.5: Abaixo do Peso
@@ -18,3 +18,13 @@ imc = (altura * altura) / peso
 #ACima dos 40: Obesidade mórbida
 
 #mostrando pro usuário
+if (imc < 18.5):
+    print('Seu IMC é {:.1f}, então você está ABAIXO DO PESO, cuidado!'.format(imc))
+elif(imc >= 18.5 and imc <= 25):
+    print('Seu IMC é {:.1f}, então você está PESO IDEAL!'.format(imc))
+elif(imc > 25 and imc <= 30):
+    print('Seu IMC é {:.1f}, então você está SOBREPESO!'.format(imc))
+elif(imc > 30 and imc <= 40):
+    print('Seu IMC é {:.1f}, então você está OBESIDADE!'.format(imc))
+else:
+    print('Seu IMC é {:.1f}, então você está OBESIDADE MORBIDA, cuidado!'.format(imc))
